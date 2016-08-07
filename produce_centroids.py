4b17c70cf4dd3c0cda7cf80a85e9a8eea55c12e6
George Brokos <g.brokos@gmail.com>
2016-08-07 21:08:30 +0300

    License headers updated.

--- produce_centroids.py
+++ produce_centroids.py
@@ -1,7 +1,4 @@
-__author__ = ("Georgios-Ioannis Brokos, "
-              "Natural Language Processing Group, "
-              "Department of Informatics, "
-              "Athens University of Economics and Business, Greece.")
+__author__ = "Georgios-Ioannis Brokos"
 __copyright__ = "Copyright (c) 2016, " + __author__
 __license__ = "3-clause BSD"
 __email__ = "g.brokos@gmail.com"
